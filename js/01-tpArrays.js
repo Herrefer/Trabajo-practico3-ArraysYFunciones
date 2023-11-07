00c90a5a1befe0147eb4f570b1bb4dda3ea32afc
Herrera Fernando <herrera770@outlook.es>
2023-11-07 13:16:37 -0300

    se agregó la solucion al problema

---
+++ js/01-tpArrays.js
@@ -0,0 +1,15 @@
+const arrayNumeros = [];
+function numeroMayor(array){
+    console.log(Math.max(...array))
+}
+
+// while(true){
+//     let numero = prompt("Introduzca un número")
+//     if (numero === null){
+//         console.log(arrayNumeros)
+//         console.log(numeroMayor(arrayNumeros))
+//         break;
+//     }else{
+//         arrayNumeros.push(numero)
+//     }
+// }
