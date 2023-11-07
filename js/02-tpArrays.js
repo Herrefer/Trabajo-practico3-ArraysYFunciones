0e08ef18cf9e4349e2e79a7a13545237059ee78e
Herrera Fernando <herrera770@outlook.es>
2023-11-07 15:42:24 -0300

    se agregaron comentarios al ejercicio 2

--- js/02-tpArrays.js
+++ js/02-tpArrays.js
@@ -1,4 +1,6 @@
-function numerosPares(arr) {
-    const listaNumeros = arr.filter(numero => numero % 2 === 0);
+function numerosPares(array) {
+    const listaNumeros = array.filter(numero => numero % 2 === 0);
     return listaNumeros;
   }
+
+//   no se si es correcto el uso del método filter
