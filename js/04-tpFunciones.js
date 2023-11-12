2ae692704c9108224aceaf142b557a5a56de2608
Herrera Fernando <herrera770@outlook.es>
2023-11-12 14:53:38 -0300

    se agregó la solución al problema

---
+++ js/04-tpFunciones.js
@@ -0,0 +1,9 @@
+function esNumeroPar(numero){
+    if(numero % 2 === 0){
+        console.log(true)
+        document.write("El número introducido es par")
+    }else{
+        console.log(false)
+        document.write("El número introducido es impar")
+    }
+}
